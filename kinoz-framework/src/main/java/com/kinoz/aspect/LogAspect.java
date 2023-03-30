be626ae644c12c1ffd917693d53fbe3dfcf5adf5
KinozHao <kinozhao0715@gmail.com>
2023-03-30 21:18:10 +0800

    blog system v1.0 keep developing!

--- kinoz-framework/src/main/java/com/kinoz/aspect/LogAspect.java
+++ kinoz-framework/src/main/java/com/kinoz/aspect/LogAspect.java
@@ -46,8 +46,6 @@ public class LogAspect {
 
     }
 
-
-
     private void handleBefore(ProceedingJoinPoint joinPoint) {
         var sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = sra.getRequest();
